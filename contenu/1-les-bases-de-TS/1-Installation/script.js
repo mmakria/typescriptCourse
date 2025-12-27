89b395100dcb42b5c8cacf12a188ece3c18bae9f
momomak <m.makria@gmail.com>
2025-12-27 23:38:40 +0100

    init ts

---
+++ contenu/1-les-bases-de-TS/1-Installation/script.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let str = "poule";
+console.log(str);
+// commande terminal --->  tsc script.ts va le compiler
+// tsc --init -----> crzation du fichier config
+// tsc -w -------> watch les changements --- cela permet d'eviter les compilations à la main
+function foo() {
+    return "test";
+}
+//# sourceMappingURL=script.js.map
---
+++ contenu/1-les-bases-de-TS/1-Installation/script.ts
@@ -0,0 +1,11 @@
+let str: string = "poule";
+
+console.log(str);
+
+// commande terminal --->  tsc script.ts va le compiler
+// tsc --init -----> crzation du fichier config
+// tsc -w -------> watch les changements --- cela permet d'eviter les compilations à la main
+
+function foo() {
+  return "test";
+}
